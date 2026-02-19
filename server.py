d3e048e1a25ab61f30ca408842984fa8e7a0d98c
Rishi <rishibellala2508@gmail.com>
2026-02-19 23:06:25 +0530

    production changes

--- server.py
+++ server.py
@@ -68,4 +68,10 @@ def ask():
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    import os
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
+
